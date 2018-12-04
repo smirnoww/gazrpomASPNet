ba4775f68beab48febeba73ab4ad37f7fdb563ea
student08 <student08@class05.cctp.ru>
2018-12-04 12:53:12 +0300

    configuration added

--- helloWorlld/Startup.cs
+++ helloWorlld/Startup.cs
@@ -5,13 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace helloWorlld
 {
     public class Startup
     {
-        string helloTxt = "Приветствует Вас!";
+        // свойство, которое будет хранить конфигурацию
+        public IConfiguration AppConfiguration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json");
+            // создаем конфигурацию
+            AppConfiguration = builder.Build();
+
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,12 +41,6 @@ namespace helloWorlld
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.Run(async (context) =>
-            //{
-            //    env.ApplicationName = "First web App";
-            //    context.Response.ContentType = "text/html; charset=utf-8";
-            //    await context.Response.WriteAsync(env.ApplicationName + " " + this.helloTxt);
-            //});
 
             app.UseMvc(routes => {
                 routes.MapRoute(
