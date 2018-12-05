34f50b39787778b8d80723c5d95d0f2dd6056060
student08 <student08@class05.cctp.ru>
2018-12-05 09:55:12 +0300

    project Views added. This one show different view depends of current time

---
+++ Views/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Views.Models;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                TimeSpan start = new TimeSpan(0, 0, 0); //0 o'clock
+                TimeSpan end = new TimeSpan(12, 0, 0); //12 o'clock
+                TimeSpan now = DateTime.Now.TimeOfDay;
+
+                if (!((now > start) && (now < end)))
+                    return View("Morning");
+                else
+                    return View("Evening");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
